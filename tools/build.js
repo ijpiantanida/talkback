ea1e252b81c0cf6f1b8eb2a70022c50d80db21c3
Ignacio Piantanida <ijpiantanida@gmail.com>
2017-12-27 18:53:31 -0300

    Add CHANGELOG

--- tools/build.js
+++ tools/build.js
@@ -40,8 +40,10 @@ promise = promise.then(() => {
   delete pkg.scripts;
   delete pkg.nyc;
   fs.writeFileSync("dist/package.json", JSON.stringify(pkg, null, "  "), "utf-8");
+  fs.writeFileSync("dist/CHANGELOG.md", fs.readFileSync("CHANGELOG.md", "utf-8"), "utf-8");
   fs.writeFileSync("dist/LICENSE.md", fs.readFileSync("LICENSE.md", "utf-8"), "utf-8");
   fs.writeFileSync("dist/README.md", fs.readFileSync("README.md", "utf-8"), "utf-8");
+
 });
 
 promise.catch(err => console.error(err.stack));

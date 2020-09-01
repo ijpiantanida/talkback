0391aa715dc86a66e2b6360ac73f4736b97d629f
Ignacio Piantanida <ijpiantanida@gmail.com>
2020-08-31 18:50:32 -0700

    Refactor tape matcher

--- src/tape-matcher.ts
+++ src/tape-matcher.ts
@@ -2,6 +2,7 @@ import ContentEncoding from "./utils/content-encoding"
 import MediaType from "./utils/media-type"
 import {Options} from "./options"
 import Tape from "./tape"
+import {Req} from "./types"
 
 const isEqual = require("lodash/isEqual")
 
@@ -17,26 +18,51 @@ export default class TapeMatcher {
   sameAs(otherTape: Tape) {
     const otherReq = otherTape.req
     const req = this.tape.req
-    const sameURL = req.url === otherReq.url
-    if (!sameURL) {
-      if (!this.options.urlMatcher) {
-        this.options.logger.debug(`Not same URL ${req.url} vs ${otherReq.url}`)
-        return false
-      }
-
-      const urlMatches = this.options.urlMatcher(this.tape, otherReq)
-      if (!urlMatches) {
-        this.options.logger.debug(`Not same urlMatcher ${req.url} vs ${otherReq.url}`)
-        return false
-      }
-    }
-
-    const sameMethod = req.method === otherReq.method
-    if (!sameMethod) {
-      this.options.logger.debug(`Not same METHOD ${req.method} vs ${otherReq.method}`)
+    if (!this.isSameUrl(req, otherReq)) {
       return false
     }
 
+    if (!this.isSameMethod(req, otherReq)) {
+      return false
+    }
+
+    if (!this.isSameHeaders(req, otherReq)) {
+      return false
+    }
+
+    return this.options.ignoreBody || this.isSameBody(req, otherReq);
+
+  }
+
+  private isSameBody(req: Req, otherReq: Req) {
+    const mediaType = new MediaType(req)
+    const contentEncoding = new ContentEncoding(req)
+
+    let sameBody: boolean
+    if (contentEncoding.isUncompressed() && mediaType.isJSON() && req.body.length > 0 && otherReq.body.length > 0) {
+      const parsedReqBody = JSON.parse(req.body.toString())
+      const parsedOtherReqBody = JSON.parse(otherReq.body.toString())
+      sameBody = isEqual(parsedReqBody, parsedOtherReqBody)
+    } else {
+      sameBody = req.body.equals(otherReq.body)
+    }
+
+    if (!sameBody) {
+      if (!this.options.bodyMatcher) {
+        this.options.logger.debug(`Not same BODY ${req.body} vs ${otherReq.body}`)
+        return false
+      }
+
+      const bodyMatches = this.options.bodyMatcher(this.tape, otherReq)
+      if (!bodyMatches) {
+        this.options.logger.debug(`Not same bodyMatcher ${req.body} vs ${otherReq.body}`)
+        return false
+      }
+    }
+    return true
+  }
+
+  private isSameHeaders(req: Req, otherReq: Req) {
     const currentHeadersLength = Object.keys(req.headers).length
     const otherHeadersLength = Object.keys(otherReq.headers).length
     const sameNumberOfHeaders = currentHeadersLength === otherHeadersLength
@@ -56,31 +82,30 @@ export default class TapeMatcher {
       this.options.logger.debug(`Not same HEADERS values ${JSON.stringify(req.headers)} vs ${JSON.stringify(otherReq.headers)}`)
       return false
     }
+    return true
+  }
 
-    if (!this.options.ignoreBody) {
-      const mediaType = new MediaType(req)
-      const contentEncoding = new ContentEncoding(req)
+  private isSameMethod(req: Req, otherReq: Req) {
+    const sameMethod = req.method === otherReq.method
+    if (!sameMethod) {
+      this.options.logger.debug(`Not same METHOD ${req.method} vs ${otherReq.method}`)
+      return false
+    }
+    return true
+  }
 
-      let sameBody = false
-      if (contentEncoding.isUncompressed() && mediaType.isJSON() && req.body.length > 0 && otherReq.body.length > 0) {
-        const parsedReqBody = JSON.parse(req.body.toString())
-        const parsedOtherReqBody = JSON.parse(otherReq.body.toString())
-        sameBody = isEqual(parsedReqBody, parsedOtherReqBody)
-      } else {
-        sameBody = req.body.equals(otherReq.body)
+  private isSameUrl(req: Req, otherReq: Req) {
+    const sameURL = req.url === otherReq.url
+    if (!sameURL) {
+      if (!this.options.urlMatcher) {
+        this.options.logger.debug(`Not same URL ${req.url} vs ${otherReq.url}`)
+        return false
       }
 
-      if (!sameBody) {
-        if (!this.options.bodyMatcher) {
-          this.options.logger.debug(`Not same BODY ${req.body} vs ${otherReq.body}`)
-          return false
-        }
-
-        const bodyMatches = this.options.bodyMatcher(this.tape, otherReq)
-        if (!bodyMatches) {
-          this.options.logger.debug(`Not same bodyMatcher ${req.body} vs ${otherReq.body}`)
-          return false
-        }
+      const urlMatches = this.options.urlMatcher(this.tape, otherReq)
+      if (!urlMatches) {
+        this.options.logger.debug(`Not same urlMatcher ${req.url} vs ${otherReq.url}`)
+        return false
       }
     }
     return true
